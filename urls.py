3d546ad0910f3286d84063d31e11d7caa6503909
Onkar Ghadigaonkar <54395843+Onkarg2002@users.noreply.github.com>
2024-07-11 14:34:07 +0530

    Main App
    Main App

--- urls.py
+++ urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('send_query/', views.send_query, name='send_query'),
+    path('admin/', admin.site.urls),
+    path('chat/', include('chat.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
